0053491573b812ca956478f7defbd42f20fbc8c4
bartomsilva <bartomsilva@gmail.com>
2023-06-23 06:49:16 -0300

    fix: correção da base url

--- front/src/constants/constants.js
+++ front/src/constants/constants.js
@@ -1,3 +1,3 @@
- export const BASE_URL_R2D = "https://romano2decimal.bartomsilva.repl.co/"
+ export const BASE_URL_R2C = "https://romano2cardinal.bartomsilva.repl.co/"
  // export const BASE_URL = "http://127.0.0.1:3003/"
  export const BASE_URL_D2E = "https://cardinal2extenso.bartomsilva.repl.co/"
